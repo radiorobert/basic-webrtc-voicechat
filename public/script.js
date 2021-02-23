6d5bbb2d9cdb36c6a5cb25792c67f92786bd0bfb
Robert Blake <robertblake@archradio.localdomain>
2021-02-23 15:39:47 -0500

    Finish basic peer connection setup
    Also fixed a small typo

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
 const socket = io('/');
+const myPeer = new Peer(undefined, {  // let server gen id
+  host: '/',
+  port: '3001'
+});
 
-socket.emit('join-room', ROOM_ID, 10);
+// establish peer connection
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
 
 socket.on('user-connected', userId => {
   console.log('User connected: ' + userId);
