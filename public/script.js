967e4237329ce714cd39c113904f0e8ced99959e
Robert Blake <robertblake@archradio.localdomain>
2021-02-23 17:45:23 -0500

    Fix displaying of users joined into the room

--- public/script.js
+++ public/script.js
@@ -6,26 +6,33 @@ const myPeer = new Peer(undefined, {  // let server gen id
   port: '3001'
 });
 
+const me = document.createElement('div');
 const myAudio = document.createElement('audio');
-myAudio.setAttribute('id', "person");
-myAudio.muted = true;  // ensure you can't hear your own audio
 
-const peers = {}
+myAudio.muted = true;  // ensure you can't hear your own audio
+me.setAttribute("id", "person");
+me.appendChild(myAudio);
+
+const peers = {};
 
 // get the media devices
 navigator.mediaDevices.getUserMedia({
   audio: true,
 }).then(stream => {
-  addAudioStream(myAudio, stream);
+  addAudioStream(me, myAudio, stream);
 
   // Answer a stream if there's a call
   myPeer.on('call', call => {
     call.answer(stream);
 
+    const person = document.createElement('div');
     const audio = document.createElement('audio');
-    audio.setAttribute("id", "person");
+
+    person.setAttribute("id", "person");
+    person.appendChild(audio);
+
     call.on('stream', userAudioStream => {
-      addAudioStream(audio, userAudioStream);
+      addAudioStream(person, audio, userAudioStream);
     });
   });
 
@@ -54,11 +61,15 @@ socket.on('user-connected', userId => {
 /* send the stream to someone, and also hear their stream */
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
+
+  const person = document.createElement('div');
   const audio = document.createElement('audio');
-  audio.setAttribute("id", "person");
+
+  person.setAttribute("id", "person");
+  person.appendChild(myAudio);
 
   call.on('stream', userAudioStream => {
-    addAudioStream(audio, userAudioStream);
+    addAudioStream(person, audio, userAudioStream);
   });
 
   call.on('close', () => {
@@ -69,10 +80,13 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call;
 }
 
-/* Add the audio to the DOM */
-function addAudioStream(audio, stream) {
+/* Add the div with audio to the DOM
+   audio doesn't have any inherent styles to it,
+   if controller is specified then can add border
+ */
+function addAudioStream(person, audio, stream) {
   audio.srcObject = stream;
   audio.addEventListener('loadedmetadata', () => { audio.play() });
 
-  userGrid.append(audio);
+  userGrid.append(person);
 }
